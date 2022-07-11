40d4fa61392d373fc3fd84f02007c2da5de36c2f
JoanaPoAlmeida <joana20011@gmail.com>
2022-07-11 02:00:42 +0100

    changes in team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -5,7 +5,6 @@ function Team() {
     <>
     <div className='container'>
         <h2>Meet The Team</h2>
-        <p>Resize the browser window to see the effect.</p>
         <br/>
 
         <div className="ro">
@@ -16,7 +15,7 @@ function Team() {
                         <h2>Joana Paulo Almeida</h2>
                         <p className="title">Student</p>
                         <p>Computer Science Student</p>
-                        <p><button className="button">LinkedIn</button></p>
+                        <p><button className="button"><a href='https://www.linkedin.com/in/joana-almeida-550260198/'>Link</a></button></p>
                     </div>
                 </div>
             </div>
@@ -28,7 +27,7 @@ function Team() {
                         <h2>Francisco Oliveira</h2>
                         <p className="title">Student</p>
                         <p>Computer Science Student</p>
-                        <p><button className="button">LinkedIn</button></p>
+                        <p><button className="button"><a href='https://www.linkedin.com/in/francisco-oliveira-2ab964195/'>Link</a></button></p>
                     </div>
                 </div>
             </div>
@@ -41,19 +40,19 @@ function Team() {
                         <h2>John Doe</h2>
                         <p className="title">Sebastião Pais</p>
                         <p>Professor Assistant at UBI</p>
-                        <p><button className="button">Contact</button></p>
+                        <p><button className="button"><a href='http://www.di.ubi.pt/~sebastiao/index.html'>Link</a></button></p>
                     </div>
                 </div>
             </div>
   
             <div className="column">
                 <div className="card">
-                <img src={require('../images/JPC.jpg')} alt="JP"/>
+                <img src={require('../images/JPC.jpg')} alt="JP" height="30%"/>
                     <div className="containr">
                         <h2>João Cordeiro</h2>
                         <p className="title">Professor</p>
                         <p>Professor Assistant at UBI</p>
-                        <p><button className="button">Contact</button></p>
+                        <p><button className="button"><a href='https://www.di.ubi.pt/~jpaulo/'>Link</a></button></p>
                     </div>
                 </div>
             </div>
@@ -65,7 +64,7 @@ function Team() {
                         <h2>Gaël Dias</h2>
                         <p className="title">Professor</p>
                         <p>Full Professor at University of Caen Normandie</p>
-                        <p><button className="button">Contact</button></p>
+                        <p><button className="button"><a href='https://dias.users.greyc.fr/'>Link</a></button></p>
                     </div>
                 </div>
             </div>
